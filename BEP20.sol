3d516a40936238cebb4b95c63f733825eae7dbc7
Prasoon180 <101745026+Prasoon180@users.noreply.github.com>
2022-07-20 12:17:40 +0530

    Delete BEP20.sol

--- BEP20.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier:GPL-3.0
-pragma solidity ^0.8.15;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract BEP20 is ERC20 {
-    constructor() ERC20("SUPERPAD", "SPAD") {
-        _mint(msg.sender, 15140000 * 10 ** 18);    // here it is initial supply or total supply
-    }
-}
